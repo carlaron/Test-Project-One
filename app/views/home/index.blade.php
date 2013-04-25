96d4f8e0986fe9711079bc77a625f2eb712110d1
ePortal <testor_eportal@commpartners.com>
2013-04-25 12:10:04 -0400

    edited home.index view

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -3,7 +3,7 @@
 EOX Home
 @stop
 @section('content')
-    <h1>EOX Pubic Home!</h1>
+    <h1>EOX Pubic Home!!</h1>
     <p>Welcome to the Home page! This is NOT an EOX client site... This will probably be marketing stuff.</p>
     <p><a href="https://demo.eportal.commpartners.com">Click HERE for the "demo" client site</a></p>
     <p><a href="https://foo.eportal.commpartners.com">Click HERE for the "foo" client site</a></p>
